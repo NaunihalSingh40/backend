ab0859fd9429ed1339dc41c1a9212f22ff6af363
Chaitanya Gupta <chaitanya.gupta3007@gmail.com>
2024-11-27 01:24:25 +0530

    working

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = express();
 require('dotenv').config();
 // require('./models/db');
 const connectDB = require('./models/db')
+const jwt = require('jsonwebtoken')
 const PORT = process.env.port || 8080;
 
 connectDB();
@@ -29,8 +30,8 @@ app.use(async (req, res, next) => {
 app.use(bodyParser.json());
 app.use(cors());
 app.use('/auth', AuthRouter);
-// app.use('/api', RoleRouter);
-app.use('/admin', AdminRouter);
+app.use('/api', RoleRouter);
+app.use('/api/admin', AdminRouter);
 app.use('/api/employee', employeeRouter)
 app.listen(PORT, () => {
     console.log(`server is running on ${PORT} port `);
